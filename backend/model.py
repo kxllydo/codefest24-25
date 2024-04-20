7d03e3e0125ba6cb9fd07f542db27aef8e0e98c1
Khang Duong <khangduong1519@gmail.com>
2024-04-20 17:31:28 -0400

    changes to data

--- backend/model.py
+++ backend/model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 import random
+import sqlite3
 
 # Load the CSV file
 data = pd.read_csv('data.csv')
@@ -17,9 +18,24 @@ clf.fit(X, y)
 
 ethnicities = ['African', 'African American', 'Arab', 'Asian', 'Caribbean', 'European', 'Hispanic/Latino', 'Indigenous', 'Middle Eastern', 'Native American', 'Pacific Islander', 'South Asian', 'Southeast Asian']
 
-test = [random.randint(1, 18), random.randint(0,1), random.randint(1, len(ethnicities)), random.randint(501, 99950), random.randint(1, 10), random.randint(1,10), random.randint(0,5), random.randint(1,4)]
+num_of_test_parents = 100
+
+test_parents = []
+for _ in range(num_of_test_parents):
+    test_parents.append([random.randint(18, 90), random.randint(0,1), random.randint(1, len(ethnicities)), random.randint(501, 99950), random.randint(0,1), random.randint(25750, 154500), random.randint(1,20), random.randint(1,10)])
 
 
+conn = sqlite3.connect('data.db')
+cursor = conn.cursor()
 
-accuracy = clf.score(X, y)
+cursor.execute("SELECT KidAge, KidGender, KidEthnicity, KidLocation, KidHobbies, KidHealthConditions, KidNumberSiblings, KidEducation FROM current_children;")
+conn.commit()
+current_children = cursor.fetchall()
+
+test_data = [list(x) + y for x, y in zip(current_children, test_parents)]
+
+
+accuracy = clf.score(test_data, y)
 print(accuracy)
+
+conn.close()

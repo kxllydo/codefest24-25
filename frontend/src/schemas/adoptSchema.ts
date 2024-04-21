27cc85d4483d972247c3d5ed6cbb2ff96297ce7f
tony <tonydrayton43@gmail.com>
2024-04-20 21:23:47 -0400

    Add backend requests to frontend and install python pkgs

--- frontend/src/schemas/adoptSchema.ts
+++ frontend/src/schemas/adoptSchema.ts
@@ -4,9 +4,9 @@ export const adoptSchema = z.object({
     gender: z.string(),
     age: z.coerce.number().gte(18, 'Must be 18 or above'),
     ethnicity: z.string(),
-    location: z.coerce.number(),
+    location: z.coerce.number().gte(9999, 'Invalid zip code'),
     marital_status: z.boolean(),
-    income: z.coerce.number(),
+    income: z.coerce.number().gte(0, 'Invalid income'),
     employed: z.boolean(),
     disabled: z.boolean()
 });

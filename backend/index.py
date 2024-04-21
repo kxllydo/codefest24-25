eb3dc5bc48023fbe3094ff10cd4e8f3346607ff2
bql23 <bql23@drexel.edu>
2024-04-20 20:18:43 -0400

    backend

--- backend/index.py
+++ backend/index.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify 
+import joblib
+from flask import request, jsonify
+
+model = joblib.load('model.pkl')
 
 app = Flask(__name__)
 
@@ -9,6 +12,23 @@ def index():
 @app.route('/api')
 def api():
     return jsonify({'message': 'Hello from server!'})
+
+
+from flask import request, jsonify
+
+@app.route('/match', methods=['POST'])
+def match():
+    data = request.json
+    age = data.get('AGE')
+    gender = data.get('GENDER')
+    ethnicity = data.get('ETHNICITY')
+    location = data.get('LOCATION')
+    marital_status = data.get('MARITAL_STATUS')
+    income = data.get('INCOME')
+    employed = data.get('EMPLOYED')
+    disabled = data.get('DISABLED')
+    
+
 
 if __name__ == '__main__':
     app.run(port=3001)
